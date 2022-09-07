f2efdd3b588ab3c132aa7991813a85fd89a9830b
nitish420 <nitishb073@gmail.com>
2022-09-08 01:42:46 +0530

    removing github icon

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,7 +12,7 @@ export default function Footer() {
 
     return (
         <div className={`${testState ? "d-none":" m-0 p-0 d-flex flex-row justify-content-center"}`} >
-            <a rel="noreferrer"  target="_blank" className='d-inline-block text-center fs-6 p-0 mx-4' href="https://github.com/nitish-420/typeIt-frontend" style={{ textDecoration:"none",cursor:"pointer",color:"#ffeba7"}}><UilGithub size="40" color="#FFD651" /> Github</a>
+            {/* <a rel="noreferrer"  target="_blank" className='d-inline-block text-center fs-6 p-0 mx-4' href="https://github.com/nitish-420/typeIt-frontend" style={{ textDecoration:"none",cursor:"pointer",color:"#ffeba7"}}><UilGithub size="40" color="#FFD651" /> Github</a> */}
             <a rel="noreferrer"  target="_blank" className='d-inline-block text-center fs-6 p-0 mx-4' href = "mailto: teamTypeIt@outlook.com" style={{textDecoration:"none",cursor:"pointer",color:"#ffeba7"}}><UilEnvelope size="43" color="#FFD651" /> TypeIt</a>
         </div>
     )
